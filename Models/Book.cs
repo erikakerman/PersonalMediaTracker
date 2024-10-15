7b4de1605ef895e43df3199c9a94675f803b526a
Erik Akerman <erikakerman@gmail.com>
2024-10-15 08:45:53 +0200

    add book feature

---
+++ Models/Book.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+class Book
+{
+    private List<string> titles = new List<string>();
+
+    public void AddTitle(string title)
+    {
+        titles.Add(title);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Book book = new Book();
+
+        Console.WriteLine("Enter a book title:");
+        string title = Console.ReadLine();
+
+        book.AddTitle(title);
+
+        Console.WriteLine("Book title added successfully!");
+    }
+}
